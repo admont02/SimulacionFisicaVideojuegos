9596561a6f90d4a5af5c99b83de9e7196a7c446b
Adrián Montero <82326212+admont02@users.noreply.github.com>
2022-12-12 21:37:13 +0100

    Create SolidForceRegistry.h

---
+++ skeleton/SolidForceRegistry.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "ForceGenerator.hpp"
+#include <map>
+
+typedef std::pair<ForceGenerator*, physx::PxRigidDynamic*> FRPair;
+
+
+class SolidForceRegistry : public std::multimap<ForceGenerator*, physx::PxRigidDynamic*> {
+public:
+	void updateForce(double duration) {
+		for (auto it = begin(); it != end(); it++)
+			it->first->updateForce(it->second, duration);
+	}
+	void addRegistry(ForceGenerator* fg, physx::PxRigidDynamic* p) {
+		this->insert(FRPair(fg, p));
+	}
+	void deleteParticleRegistry(physx::PxRigidDynamic* p) {
+		auto it = begin();
+		while (it != end()) {
+			if (it->second != p) it++;
+			else it = this->erase(it);
+		}
+	}
+};
